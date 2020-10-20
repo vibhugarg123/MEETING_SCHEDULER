e59d024f28fe6db39a286ad44f83685c698323ea
Vibhu.Garg <vibhu.garg@ril.com>
2020-10-21 02:04:42 +0530

    Initial Commmit

---
+++ meetingscheduler/src/main/java/com/gojek/meetingscheduler/MeetingschedulerApplication.java
@@ -0,0 +1,13 @@
+package com.gojek.meetingscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingschedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingschedulerApplication.class, args);
+	}
+
+}
---
+++ meetingscheduler/src/test/java/com/gojek/meetingscheduler/MeetingschedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gojek.meetingscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingschedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
